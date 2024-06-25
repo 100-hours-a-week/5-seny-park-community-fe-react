6d0a6f6ca1dc8bb187d74be1aaa28526838d03bf
sen2y <pse6704@sookmyung.ac.kr>
2024-06-25 15:08:46 +0900

    🎨 헤더 컴포넌트 구현 및 공통 레이아웃 지정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; 
-import App from './App'; 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Header } from "./components/organisms/Header";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>
 );
- 
