6036a1b51f939fdb1f6e87623e416af095ce5ca7
sen2y <pse6704@sookmyung.ac.kr>
2024-06-25 14:53:09 +0900

    Revert "💄 버튼 컴포넌트"
    This reverts commit c17fed19946515a515a999001cc9719006137e23.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './App.css';
 import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </Router>
+  <div>
+    <SignIn/>
+  </div>
   );
 }
 
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,94 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
-import { validateEmail } from "../utils/validation";
-import { HELPER_TEXT } from "../constants/helperText";
-import SignInForm from "../components/organisms/SignInForm";
 
-const SignIn = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  const [isValid, setIsValid] = useState(false);
-
-  const validateForm = () => {
-    const emailValid = validateEmail(email);
-    const passwordValid = password.length > 0;
-    return emailValid && passwordValid;
-  };
-
-  useEffect(() => {
-    setIsValid(validateForm());
-  }, [email, password]);
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-    if (!e.target.value) {
-      setEmailError(HELPER_TEXT.EMAIL_EMPTY);
-    } else if (!validateEmail(e.target.value)) {
-      setEmailError(HELPER_TEXT.EMAIL_VALIDATION_FALSE);
-    } else {
-      setEmailError("");
-    }
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-    if (!e.target.value) {
-      setPasswordError(HELPER_TEXT.PASSWORD_EMPTY);
-    } else {
-      setPasswordError("");
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    let valid = true;
-
-    if (!email) {
-      setEmailError(HELPER_TEXT.EMAIL_EMPTY);
-      valid = false;
-    } else if (!validateEmail(email)) {
-      setEmailError(HELPER_TEXT.EMAIL_VALIDATION_FALSE);
-      valid = false;
-    } else {
-      setEmailError("");
-    }
-
-    if (!password) {
-      setPasswordError(HELPER_TEXT.PASSWORD_EMPTY);
-      valid = false;
-    } else {
-      setPasswordError("");
-    }
-
-    if (valid) {
-      // 유효성 검사가 통과된 경우 로그인 로직을 수행합니다.
-      console.log("로그인 성공!");
-    }
-  };
-
+export function SignIn() {
   return (
-    <Container>
-      <SignInForm
-        email={email}
-        password={password}
-        emailError={emailError}
-        passwordError={passwordError}
-        handleEmailChange={handleEmailChange}
-        handlePasswordChange={handlePasswordChange}
-        handleSubmit={handleSubmit}
-        isValid={isValid}
-      />
-    </Container>
+    <SignInContainer>
+      <SignInForm>
+        <SignInTitle>아무말 대잔치</SignInTitle>
+        <SignInInput placeholder="Email" />
+        <SignInInput placeholder="Password" />
+        <SignInButton>Sign In</SignInButton>
+      </SignInForm>
+    </SignInContainer>
   );
-};
-const Container = styled.div`
+}
+
+const SignInContainer = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 100px 0;
+  height: 100vh;
+`;
+
+const SignInForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+`;  
+
+const SignInTitle = styled.h1`
+  font-size: 24px;
+  font-weight: 700;
+  margin-bottom: 24px;
+`;  
+
+const SignInInput = styled.input`
+  width: 100%;
+  height: 44px;
+  border: 1px solid #dbdbdb;
+  border-radius: 4px;
+  padding: 0 8px;
+  margin-bottom: 8px;
+`;
+
+const SignInButton = styled.button`
+  width: 100%;
+  height: 44px;
+  background-color: #0095f6;
+  color: white;
+  font-weight: 600;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
 `;
 
 export default SignIn;
